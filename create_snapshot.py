515517ab4f86b564ae3259e7d5ffc93403094894
Anurag Mathur <anuragmathur1@yahoo.com>
2016-08-07 03:47:53 +0000

    add filters to create-snapshot python script to take backup only for volumes attached to running instances

--- create_snapshot.py
+++ create_snapshot.py
@@ -3,8 +3,16 @@ import boto3
 
 ec2 = boto3.resource('ec2')
 
-for volume in ec2.volumes.all():
-	vol_id = volume.id
-	print vol_id
-	description = "backup-%s" %(vol_id)
-	ec2.create_snapshot(VolumeId=vol_id, Description=description)
+print "\n\nAWS snapshot backup started"
+instances = ec2.instances.filter(
+	Filters = [{'Name':'instance-state-name', 'Values':['running']}])
+for instance in instances:
+	instance_name = filter(lambda tag: tag['Key'] == 'Name', instance.tags)[0]['Value']
+	
+	for volume in ec2.volumes.filter(Filters=[{'Name':'attachment.instance-id', 'Values':[instance.id]}]):
+		description = 'scheduled_snapshot-%s.%s' % (instance_name, volume.volume_id)
+
+	if volume.create_snapshot(VolumeId=volume.volume_id, Description=description):
+		print "Snapshot created with description [%s]" %description
+
+print "\n\nAWS snapshot backups completed"
